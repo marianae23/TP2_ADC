2aed85338327c96070ed844466c790772439048c
marianae23 <marianaenachi23@gmail.com>
2026-04-21 11:28:18 +0100

    Criação do produto bolo de chocolate e os seus dados

---
+++ produtos.py
@@ -0,0 +1,17 @@
+produtos = [
+    {
+    "id": 1,
+    "nome": "Bolo de Chocolate",
+    "categoria": "bolo",
+    "preco": 25.0,
+    "stock": 10,
+    "disponivel": True,
+    "sabores": ["chocolate"],
+    "tamanhos": ["pequeno", "medio", "grande"],
+    "cores": ["castanho"],
+    "intolerancias": ["sem glúten"],
+    "promocao": None,
+    "novo": False,
+    "avaliacoes": []
+    }
+]
