f83d60c999cf437c15f87dd1168802e5675321bf
marianae23 <marianaenachi23@gmail.com>
2026-04-21 11:37:17 +0100

    Criação do produto cupcake de morango

---
+++ dados.py
@@ -0,0 +1,31 @@
+produtos = [
+    {
+        "id": 1,
+        "nome": "Bolo de Chocolate",
+        "categoria": "bolo",
+        "preco": 25.0,
+        "stock": 10,
+        "disponivel": True,
+        "sabores": ["chocolate"],
+        "tamanhos": ["pequeno", "medio", "grande"],
+        "cores": ["castanho"],
+        "intolerancias": ["sem glúten"],
+        "promocao": None,
+        "novo": False,
+        "avaliacoes": []
+    },
+    {
+        "id": 2,
+        "nome": "Cupcake de Morango",
+        "categoria": "Cupcake",
+        "preco": 3.5,
+        "disponivel": True,
+        "sabores": ["morango"],
+        "tamanhos": ["unico"],
+        "cores": ["rosa"],
+        "intolerancias": [],
+        "novo": False,
+        "promocao": None,
+        "avaliacoes": []
+    }
+]
