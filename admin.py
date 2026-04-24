19ffe9919ca70c4c9078a99ac4d9b9ea2ad6a7a7
luizateixeiraualg <a86826@ualg.pt>
2026-04-24 13:12:53 +0100

    alterar estado de encomenda

---
+++ admin.py
@@ -0,0 +1,17 @@
+from produtos import produtos
+from encomendas import encomendas
+def ordenar_por_data ()
+    return sorted(key=lamba, e: "data_desejada")
+
+def mostrar_produtos()
+    return produtos
+
+def alterar_estado_encomenda()
+
+def verificar_stock()
+
+def adicionar_produto()
+
+def remover_produto()
+
+def alterar_preços()
